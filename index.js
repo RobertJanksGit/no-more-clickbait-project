e04f852656e861b1c6683428e8cb752048c23673
Robert Jank <robert.jank@yahoo.com>
2024-09-04 22:07:36 -0500

    added cors

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const OpenAI = require("openai");
 
 const express = require("express");
+const cors = require("cors");
 const app = express();
 const PORT = 3000;
+
+app.use(cors());
 
 require("dotenv").config();
 
